28e742f81466c395d82fb5208d7f80e0b66b53c4
Edward Barrier III <edwardbarrieriii@outlook.com>
2025-04-10 21:06:25 -0500

    Add project files.

---
+++ ApiPlayground.Data/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiPlayground.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPlayground.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Blog> Blog { get; set; }
+    public DbSet<Post> Post { get; set; }
+}
